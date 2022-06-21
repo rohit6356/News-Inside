d1e3f91ec80552098dc61f121db0ab02ff966904
Rohit <sharmarohit992983@gmail.com>
2022-06-21 10:51:31 -0700

    add infinite scroll in news-inside

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Newsitem extends Component {
   render() {
 
-    let {title , description , imageUrl , newsUrl} = this.props;
+    let {title , description , imageUrl , newsUrl , author ,time}  = this.props;
 
     return (
       <div className="my-3">
@@ -12,6 +12,7 @@ export default class Newsitem extends Component {
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{description}</p>
+   <p>by {author?author:"Unknown"} at {new Date(time).toGMTString()}</p>
     <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-dark">Read more  . .</a>
   </div>
 </div>
